e26f62577f44cb3cbefefc8bffd0c5e5169c0ca9
henan.wang <henan.wang@henan.wang's MacBook Pro>
2019-01-15 17:36:03 +0800

    change flag

--- demo_texture.py
+++ demo_texture.py
@@ -20,63 +20,71 @@ def texture_editing(prn, args):
     image = imread(args.image_path)
     [h, w, _] = image.shape
 
-    #-- 1. 3d reconstruction -> get texture. 
-    pos = prn.process(image) 
+    # -- 1. 3d reconstruction -> get texture.
+    pos = prn.process(image)
     vertices = prn.get_vertices(pos)
-    image = image/255.
-    texture = cv2.remap(image, pos[:,:,:2].astype(np.float32), None, interpolation=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT,borderValue=(0))
-    
-    #-- 2. Texture Editing
+    image = image / 255.
+    texture = cv2.remap(image, pos[:, :, :2].astype(np.float32), None, interpolation=cv2.INTER_NEAREST,
+                        borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
+
+    # -- 2. Texture Editing
     Mode = args.mode
     # change part of texture(for data augumentation/selfie editing. Here modify eyes for example)
-    if Mode == 0: 
+    if Mode == 0:
         # load eye mask
-        uv_face_eye = imread('Data/uv-data/uv_face_eyes.png', as_grey=True)/255. 
-        uv_face = imread('Data/uv-data/uv_face.png', as_grey=True)/255.
+        uv_face_eye = imread('Data/uv-data/uv_face_eyes.png', as_grey=True) / 255.
+        uv_face = imread('Data/uv-data/uv_face.png', as_grey=True) / 255.
         eye_mask = (abs(uv_face_eye - uv_face) > 0).astype(np.float32)
 
         # texture from another image or a processed texture
         ref_image = imread(args.ref_path)
         ref_pos = prn.process(ref_image)
-        ref_image = ref_image/255.
-        ref_texture = cv2.remap(ref_image, ref_pos[:,:,:2].astype(np.float32), None, interpolation=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT,borderValue=(0))
+        ref_image = ref_image / 255.
+        ref_texture = cv2.remap(ref_image, ref_pos[:, :, :2].astype(np.float32), None, interpolation=cv2.INTER_NEAREST,
+                                borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
 
         # modify texture
-        new_texture = texture*(1 - eye_mask[:,:,np.newaxis]) + ref_texture*eye_mask[:,:,np.newaxis]
-    
+        new_texture = texture * (1 - eye_mask[:, :, np.newaxis]) + ref_texture * eye_mask[:, :, np.newaxis]
+
     # change whole face(face swap)
-    elif Mode == 1: 
+    elif Mode == 1:
         # texture from another image or a processed texture
         ref_image = imread(args.ref_path)
         ref_pos = prn.process(ref_image)
-        ref_image = ref_image/255.
-        ref_texture = cv2.remap(ref_image, ref_pos[:,:,:2].astype(np.float32), None, interpolation=cv2.INTER_NEAREST, borderMode=cv2.BORDER_CONSTANT,borderValue=(0))
+        ref_image = ref_image / 255.
+        ref_texture = cv2.remap(ref_image, ref_pos[:, :, :2].astype(np.float32), None, interpolation=cv2.INTER_NEAREST,
+                                borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
         ref_vertices = prn.get_vertices(ref_pos)
-        new_texture = ref_texture#(texture + ref_texture)/2.
+        new_texture = ref_texture  # (texture + ref_texture)/2.
 
     else:
         print('Wrong Mode! Mode should be 0 or 1.')
         exit()
 
-
-    #-- 3. remap to input image.(render)
+    # -- 3. remap to input image.(render)
     vis_colors = np.ones((vertices.shape[0], 1))
-    face_mask = render_texture(vertices.T, vis_colors.T, prn.triangles.T, h, w, c = 1)
+    face_mask = render_texture(vertices.T, vis_colors.T, prn.triangles.T, h, w, c=1)
     face_mask = np.squeeze(face_mask > 0).astype(np.float32)
-    
+
     new_colors = prn.get_colors_from_texture(new_texture)
-    new_image = render_texture(vertices.T, new_colors.T, prn.triangles.T, h, w, c = 3)
-    new_image = image*(1 - face_mask[:,:,np.newaxis]) + new_image*face_mask[:,:,np.newaxis]
+    new_image = render_texture(vertices.T, new_colors.T, prn.triangles.T, h, w, c=3)
+    new_image = image * (1 - face_mask[:, :, np.newaxis]) + new_image * face_mask[:, :, np.newaxis]
 
     # Possion Editing for blending image
-    vis_ind = np.argwhere(face_mask>0)
+    vis_ind = np.argwhere(face_mask > 0)
     vis_min = np.min(vis_ind, 0)
     vis_max = np.max(vis_ind, 0)
-    center = (int((vis_min[1] + vis_max[1])/2+0.5), int((vis_min[0] + vis_max[0])/2+0.5))
-    output = cv2.seamlessClone((new_image*255).astype(np.uint8), (image*255).astype(np.uint8), (face_mask*255).astype(np.uint8), center, cv2.NORMAL_CLONE)
-   
+    center = (int((vis_min[1] + vis_max[1]) / 2 + 0.5), int((vis_min[0] + vis_max[0]) / 2 + 0.5))
+    imsave(args.output_path[:-4]+'_new_image'+args.output_path[-4:], new_image)
+    imsave(args.output_path[:-4]+'_image'+args.output_path[-4:], image)
+    imsave(args.output_path[:-4]+'_face_mask'+args.output_path[-4:], face_mask)
+    output = cv2.seamlessClone((new_image * 255).astype(np.uint8), (image * 255).astype(np.uint8),
+                               # (face_mask * 255).astype(np.uint8), center, cv2.NORMAL_CLONE)
+                               (face_mask * 255).astype(np.uint8), center, cv2.MIXED_CLONE)
+                               # (face_mask * 255).astype(np.uint8), center, cv2.MONOCHROME_TRANSFER)
+
     # save output
-    imsave(args.output_path, output) 
+    imsave(args.output_path, output)
     print('Done.')
 
 
@@ -85,17 +93,17 @@ if __name__ == '__main__':
 
     parser.add_argument('-i', '--image_path', default='TestImages/AFLW2000/image00081.jpg', type=str,
                         help='path to input image')
-    parser.add_argument('-r', '--ref_path', default='TestImages/trump.jpg', type=str, 
+    parser.add_argument('-r', '--ref_path', default='TestImages/trump.jpg', type=str,
                         help='path to reference image(texture ref)')
-    parser.add_argument('-o', '--output_path', default='TestImages/output.jpg', type=str, 
+    parser.add_argument('-o', '--output_path', default='TestImages/output.jpg', type=str,
                         help='path to save output')
-    parser.add_argument('--mode', default=1, type=int, 
+    parser.add_argument('--mode', default=1, type=int,
                         help='ways to edit texture. 0 for modifying parts, 1 for changing whole')
-    parser.add_argument('--gpu', default='0', type=str, 
+    parser.add_argument('--gpu', default='0', type=str,
                         help='set gpu id, -1 for CPU')
 
     # ---- init PRN
-    os.environ['CUDA_VISIBLE_DEVICES'] = parser.parse_args().gpu # GPU number, -1 for CPU
-    prn = PRN(is_dlib = True) 
+    os.environ['CUDA_VISIBLE_DEVICES'] = parser.parse_args().gpu  # GPU number, -1 for CPU
+    prn = PRN(is_dlib=True)
 
     texture_editing(prn, parser.parse_args())
--- demo_texture.sh
+++ demo_texture.sh
@@ -9,8 +9,8 @@
 #python demo_texture.py -i TestImages/4.jpg -r TestImages/0.jpg -o TestImages/4_0.jpg
 #python demo_texture.py -i TestImages/0.jpg -r TestImages/4.jpg -o TestImages/0_4.jpg
 
-#python demo_texture.py -i TestImages/image.jpg -r TestImages/0.jpg -o TestImages/image_0.jpg
-#python demo_texture.py -i TestImages/0.jpg -r TestImages/image.jpg -o TestImages/0_image.jpg
+python demo_texture.py -i TestImages/image.jpg -r TestImages/0.jpg -o TestImages/image_0.jpg
+python demo_texture.py -i TestImages/0.jpg -r TestImages/image.jpg -o TestImages/0_image.jpg
 
 python demo_texture.py -i TestImages/image.jpg -r TestImages/trump.jpg -o TestImages/image_trump.jpg
 python demo_texture.py -i TestImages/trump.jpg -r TestImages/image.jpg -o TestImages/trump_image.jpg
