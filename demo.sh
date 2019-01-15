4ff663695e1a05fba0e9845bfc1619e93c09b955
henan.wang <henan.wang@henan.wang's MacBook Pro>
2019-01-15 15:25:19 +0800

    add script

---
+++ demo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python demo.py -i TestImages -o TestImagesResult --isDlib True
+
---
+++ demo_texture.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#python demo_texture.py -i TestImages/0.jpg -r TestImages/trump.jpg -o TestImages/0_trump.jpg
+#python demo_texture.py -i TestImages/trump.jpg -r TestImages/0.jpg -o TestImages/trump_0.jpg
+
+#python demo_texture.py -i TestImages/4.jpg -r TestImages/trump.jpg -o TestImages/4_trump.jpg
+#python demo_texture.py -i TestImages/trump.jpg -r TestImages/4.jpg -o TestImages/trump_4.jpg
+
+#python demo_texture.py -i TestImages/4.jpg -r TestImages/0.jpg -o TestImages/4_0.jpg
+#python demo_texture.py -i TestImages/0.jpg -r TestImages/4.jpg -o TestImages/0_4.jpg
+
+#python demo_texture.py -i TestImages/image.jpg -r TestImages/0.jpg -o TestImages/image_0.jpg
+#python demo_texture.py -i TestImages/0.jpg -r TestImages/image.jpg -o TestImages/0_image.jpg
+
+python demo_texture.py -i TestImages/image.jpg -r TestImages/trump.jpg -o TestImages/image_trump.jpg
+python demo_texture.py -i TestImages/trump.jpg -r TestImages/image.jpg -o TestImages/trump_image.jpg
